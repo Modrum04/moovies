95d470f12fa1c1d000d7cf3929262ffd37b66e3b
Modrum <tristanlaroye@hotmail.com>
2024-06-01 08:59:27 +0200

    delete com

--- src/pages/SearchArea.jsx
+++ src/pages/SearchArea.jsx
@@ -1,37 +1,16 @@
-import { useState } from "react";
 import "./SearchPage.scss";
 import "./SearchArea.scss";
-import Filter from "./Filter";
-import SearchPage from "./SearchPage";
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 function SearchArea() {
-  const [isDiscover, setIsDiscover] = useState(false);
-
   return (
     <div className="container-search-area">
       <div className="selector-search-system-container">
-        <NavLink to="search">
-          {/* <button
-            className={`${!isDiscover ? "active-tab" : ""}`}
-            onClick={() => setIsDiscover(false)}
-          > */}
-          Rechercher
-          {/* </button> */}
-        </NavLink>
-        <NavLink to="filter">
-          {" "}
-          {/* <button
-            className={`${isDiscover ? "active-tab" : ""}`}
-            onClick={() => setIsDiscover(true)}
-          > */}
-          Découvrir
-          {/* </button> */}
-        </NavLink>
+        <NavLink to="search">Rechercher</NavLink>
+        <NavLink to="filter">Découvrir</NavLink>
       </div>
       <div className="separator"></div>
       <Outlet />
-      {/* {isDiscover ? <Filter /> : <SearchPage />} */}
     </div>
   );
 }
